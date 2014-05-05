cb6916090dedd598de2e4d3940be756367a2ec22
Ichinose Shogo <shogo82148@gmail.com>
2014-05-05 12:23:09 +0900

    ResValueをcommon.goへ移動

--- common.go
+++ common.go
@@ -60,6 +60,34 @@ const NilResStringPoolRef = ResStringPoolRef(0xFFFFFFFF)
 
 type ResStringPoolRef uint32
 
+const (
+	TYPE_NULL            = 0x00
+	TYPE_REFERENCE       = 0x01
+	TYPE_ATTRIBUTE       = 0x02
+	TYPE_STRING          = 0x03
+	TYPE_FLOAT           = 0x04
+	TYPE_DIMENSION       = 0x05
+	TYPE_FRACTION        = 0x06
+	TYPE_FIRST_INT       = 0x10
+	TYPE_INT_DEC         = 0x10
+	TYPE_INT_HEX         = 0x11
+	TYPE_INT_BOOLEAN     = 0x12
+	TYPE_FIRST_COLOR_INT = 0x1c
+	TYPE_INT_COLOR_ARGB8 = 0x1c
+	TYPE_INT_COLOR_RGB8  = 0x1d
+	TYPE_INT_COLOR_ARGB4 = 0x1e
+	TYPE_INT_COLOR_RGB4  = 0x1f
+	TYPE_LAST_COLOR_INT  = 0x1f
+	TYPE_LAST_INT        = 0x1f
+)
+
+type ResValue struct {
+	Size     uint16
+	Res0     uint8
+	DataType uint8
+	Data     uint32
+}
+
 func readStringPool(sr *io.SectionReader) (*ResStringPool, error) {
 	sp := new(ResStringPool)
 	if err := binary.Read(sr, binary.LittleEndian, &sp.Header); err != nil {
