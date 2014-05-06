edbb06d53431b57c2312ed739e8cf2fa966e4099
Ichinose Shogo <shogo82148@gmail.com>
2014-05-06 16:34:01 +0900

    XMLを開く処理を追加

--- xml.go
+++ xml.go
@@ -3,6 +3,7 @@ package androidbinary
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"os"
@@ -48,6 +49,60 @@ type ResXMLTreeAttribute struct {
 type ResXMLTreeEndElementExt struct {
 	NS   ResStringPoolRef
 	Name ResStringPoolRef
+}
+
+func NewXMLFile(r io.ReaderAt) (*XMLFile, error) {
+	f := new(XMLFile)
+	sr := io.NewSectionReader(r, 0, 1<<63-1)
+
+	fmt.Fprintf(&f.xmlBuffer, xml.Header)
+
+	header := new(ResChunkHeader)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return nil, err
+	}
+	offset := int64(header.HeaderSize)
+	for offset < int64(header.Size) {
+		chunkHeader, err := f.readChunk(r, offset)
+		if err != nil {
+			return nil, err
+		}
+		offset += int64(chunkHeader.Size)
+	}
+	return f, nil
+}
+
+func (f *XMLFile) Reader() *bytes.Reader {
+	return bytes.NewReader(f.xmlBuffer.Bytes())
+}
+
+func (f *XMLFile) readChunk(r io.ReaderAt, offset int64) (*ResChunkHeader, error) {
+	sr := io.NewSectionReader(r, offset, 1<<63-1-offset)
+	chunkHeader := &ResChunkHeader{}
+	sr.Seek(0, os.SEEK_SET)
+	if err := binary.Read(sr, binary.LittleEndian, chunkHeader); err != nil {
+		return nil, err
+	}
+
+	var err error
+	sr.Seek(0, os.SEEK_SET)
+	switch chunkHeader.Type {
+	case RES_STRING_POOL_TYPE:
+		f.stringPool, err = readStringPool(sr)
+	case RES_XML_START_NAMESPACE_TYPE:
+		err = f.readStartNamespace(sr)
+	case RES_XML_END_NAMESPACE_TYPE:
+		err = f.readEndNamespace(sr)
+	case RES_XML_START_ELEMENT_TYPE:
+		err = f.readStartElement(sr)
+	case RES_XML_END_ELEMENT_TYPE:
+		err = f.readEndElement(sr)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return chunkHeader, nil
 }
 
 func (f *XMLFile) GetString(ref ResStringPoolRef) string {
@@ -101,4 +156,83 @@ func (f *XMLFile) addNamespacePrefix(ns, name ResStringPoolRef) string {
 	} else {
 		return f.GetString(name)
 	}
+}
+
+func (f *XMLFile) readStartElement(sr *io.SectionReader) error {
+	header := new(ResXMLTreeNode)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return err
+	}
+
+	sr.Seek(int64(header.Header.HeaderSize), os.SEEK_SET)
+	ext := new(ResXMLTreeAttrExt)
+	if err := binary.Read(sr, binary.LittleEndian, ext); err != nil {
+		return nil
+	}
+
+	fmt.Fprintf(&f.xmlBuffer, "<%s", f.addNamespacePrefix(ext.NS, ext.Name))
+
+	// output XML namespaces
+	if f.notPrecessedNS != nil {
+		for uri, prefix := range f.notPrecessedNS {
+			fmt.Fprintf(&f.xmlBuffer, " xmlns:%s=\"", f.GetString(prefix))
+			xml.Escape(&f.xmlBuffer, []byte(f.GetString(uri)))
+			fmt.Fprint(&f.xmlBuffer, "\"")
+		}
+		f.notPrecessedNS = nil
+	}
+
+	// process attributes
+	offset := int64(ext.AttributeStart + header.Header.HeaderSize)
+	for i := 0; i < int(ext.AttributeCount); i++ {
+		sr.Seek(offset, os.SEEK_SET)
+		attr := new(ResXMLTreeAttribute)
+		binary.Read(sr, binary.LittleEndian, attr)
+
+		var value string
+		if attr.RawValue != NilResStringPoolRef {
+			value = f.GetString(attr.RawValue)
+		} else {
+			data := attr.TypedValue.Data
+			switch attr.TypedValue.DataType {
+			case TYPE_NULL:
+				value = ""
+			case TYPE_REFERENCE:
+				value = fmt.Sprintf("@0x%08X", data)
+			case TYPE_INT_DEC:
+				value = fmt.Sprintf("%d", data)
+			case TYPE_INT_HEX:
+				value = fmt.Sprintf("0x%08X", data)
+			case TYPE_INT_BOOLEAN:
+				if data != 0 {
+					value = "true"
+				} else {
+					value = "false"
+				}
+			default:
+				value = fmt.Sprintf("@0x%08X", data)
+			}
+		}
+
+		fmt.Fprintf(&f.xmlBuffer, " %s=\"", f.addNamespacePrefix(attr.NS, attr.Name))
+		xml.Escape(&f.xmlBuffer, []byte(value))
+		fmt.Fprint(&f.xmlBuffer, "\"")
+		offset += int64(ext.AttributeSize)
+	}
+	fmt.Fprint(&f.xmlBuffer, ">")
+	return nil
+}
+
+func (f *XMLFile) readEndElement(sr *io.SectionReader) error {
+	header := new(ResXMLTreeNode)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return err
+	}
+	sr.Seek(int64(header.Header.HeaderSize), os.SEEK_SET)
+	ext := new(ResXMLTreeEndElementExt)
+	if err := binary.Read(sr, binary.LittleEndian, ext); err != nil {
+		return err
+	}
+	fmt.Fprintf(&f.xmlBuffer, "</%s>", f.addNamespacePrefix(ext.NS, ext.Name))
+	return nil
 }
--- xml_test.go
+++ xml_test.go
@@ -2,9 +2,108 @@ package androidbinary
 
 import (
 	"bytes"
+	"encoding/xml"
 	"io"
+	"os"
+	"reflect"
 	"testing"
 )
+
+type XMLManifest struct {
+	XMLName         xml.Name             `xml:"manifest"`
+	VersionName     string               `xml:"http://schemas.android.com/apk/res/android versionName,attr"`
+	VersionCode     string               `xml:"http://schemas.android.com/apk/res/android versionCode,attr"`
+	Package         string               `xml:"package,attr"`
+	UsesPermissions []*XMLUsesPermission `xml:"uses-permission"`
+	Applications    []*XMLApplication    `xml:"application"`
+}
+
+type XMLUsesPermission struct {
+	XMLName xml.Name `xml:"uses-permission"`
+	Name    string   `xml:"http://schemas.android.com/apk/res/android name,attr"`
+}
+
+type XMLApplication struct {
+	XMLName       xml.Name          `xml:"application"`
+	Label         string            `xml:"http://schemas.android.com/apk/res/android label,attr"`
+	Icon          string            `xml:"http://schemas.android.com/apk/res/android icon,attr"`
+	Debuggable    string            `xml:"http://schemas.android.com/apk/res/android debuggable,attr"`
+	UsesLibraries []*XMLUsesLibrary `xml:"uses-library"`
+	Activities    []*XMLActivity    `xml:"activity"`
+}
+
+type XMLUsesLibrary struct {
+	XMLName xml.Name `xml:"uses-library"`
+	Name    string   `xml:"http://schemas.android.com/apk/res/android name,attr"`
+}
+
+type XMLActivity struct {
+	XMLName           xml.Name `xml:"activity"`
+	Name              string   `xml:"http://schemas.android.com/apk/res/android name,attr"`
+	ScreenOrientation string   `xml:"http://schemas.android.com/apk/res/android screenOrientation,attr"`
+}
+
+func TestNewXMLFile(t *testing.T) {
+	f, _ := os.Open("testdata/AndroidManifest.xml")
+	xmlFile, err := NewXMLFile(f)
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+	decoder := xml.NewDecoder(xmlFile.Reader())
+	xmlManifest := &XMLManifest{}
+	err = decoder.Decode(xmlManifest)
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+	expected := &XMLManifest{
+		XMLName:     xml.Name{"", "manifest"},
+		VersionName: "テスト版",
+		VersionCode: "1",
+		Package:     "net.sorablue.shogo.FWMeasure",
+		UsesPermissions: []*XMLUsesPermission{
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.CAMERA"},
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.WAKE_LOCK"},
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.ACCESS_FINE_LOCATION"},
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.INTERNET"},
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.ACCESS_MOCK_LOCATION"},
+			{XMLName: xml.Name{"", "uses-permission"}, Name: "android.permission.RECORD_AUDIO"},
+		},
+		Applications: []*XMLApplication{
+			{
+				XMLName:    xml.Name{"", "application"},
+				Label:      "@0x7F040000",
+				Icon:       "@0x7F020000",
+				Debuggable: "false",
+				UsesLibraries: []*XMLUsesLibrary{
+					{XMLName: xml.Name{"", "uses-library"}, Name: "com.google.android.maps"},
+				},
+				Activities: []*XMLActivity{
+					{
+						XMLName:           xml.Name{"", "activity"},
+						ScreenOrientation: "0",
+						Name:              "FWMeasureActivity",
+					},
+					{
+						XMLName:           xml.Name{"", "activity"},
+						ScreenOrientation: "0",
+						Name:              "MapActivity",
+					},
+					{
+						XMLName: xml.Name{"", "activity"},
+						Name:    "SettingActivity",
+					},
+					{
+						XMLName: xml.Name{"", "activity"},
+						Name:    "PlaceSettingActivity",
+					},
+				},
+			},
+		},
+	}
+	if !reflect.DeepEqual(xmlManifest, expected) {
+		t.Errorf("got %v want %v", xmlManifest, expected)
+	}
+}
 
 func TestReadStartNamespace(t *testing.T) {
 	input := []uint8{
@@ -74,5 +173,81 @@ func TestAddNamespacePrefix(t *testing.T) {
 
 	if actual := f.addNamespacePrefix(uriRef, nameRef); actual != "prefix:name" {
 		t.Errorf("got %v want prefix:name", actual)
+	}
+}
+
+func TestReadStartElement(t *testing.T) {
+	input := []uint8{
+		0x02, 0x01, // Type = RES_XML_START_ELEMENT_TYPE
+		0x10, 0x00, // HeadSize = 16 bytes
+		0x3C, 0x00, 0x00, 0x00, // Size = 60 bytes
+		0x01, 0x00, 0x00, 0x00, // LineNumber = 1
+		0xFF, 0xFF, 0xFF, 0xFF, // Comment is none
+		0xFF, 0xFF, 0xFF, 0xFF, // Namespace is none
+		0x01, 0x00, 0x00, 0x00, // Name = 1
+		0x14, 0x00, // AttributeStart = 20 bytes
+		0x14, 0x00, // AttributeSize = 20 bytes
+		0x01, 0x00, // AttributeCount = 1
+		0x00, 0x00, // IdIndex = 0
+		0x00, 0x00, // ClassIndex = 0
+		0x00, 0x00, // StyleIndex = 0
+
+		// Attributes
+		0xFF, 0xFF, 0xFF, 0xFF, // Namespace is none
+		0x04, 0x00, 0x00, 0x00, // Name is 'attr'
+		0x05, 0x00, 0x00, 0x00, // RawValies is 'value'
+		0x08, 0x00, // size = 8
+		0x00,                   // padding
+		0x03,                   // data type is TYPE_STRING
+		0x05, 0x00, 0x00, 0x00, // data
+	}
+	buf := bytes.NewReader(input)
+	sr := io.NewSectionReader(buf, 0, int64(len(input)))
+
+	prefixRef := ResStringPoolRef(2)
+	uriRef := ResStringPoolRef(3)
+
+	f := new(XMLFile)
+	f.notPrecessedNS = make(map[ResStringPoolRef]ResStringPoolRef)
+	f.notPrecessedNS[uriRef] = prefixRef
+	f.namespaces = make(map[ResStringPoolRef]ResStringPoolRef)
+	f.namespaces[uriRef] = prefixRef
+	f.stringPool = new(ResStringPool)
+	f.stringPool.Strings = []string{"", "name", "prefix", "http://example.com", "attr", "value"}
+	err := f.readStartElement(sr)
+
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+
+	if actual := f.xmlBuffer.String(); actual != "<name xmlns:prefix=\"http://example.com\" attr=\"value\">" {
+		t.Errorf("got %v want <name xmlns:prefix=\"http://example.com\" attr=\"value\">", actual)
+	}
+}
+
+func TestReadEndElement(t *testing.T) {
+	input := []uint8{
+		0x03, 0x01, // Type = RES_XML_END_ELEMENT_TYPE
+		0x10, 0x00, // HeadSize = 16 bytes
+		0x18, 0x00, 0x00, 0x00, // Size
+		0x01, 0x00, 0x00, 0x00, // LineNumber = 1
+		0xFF, 0xFF, 0xFF, 0xFF, // Comment is none
+		0xFF, 0xFF, 0xFF, 0xFF, // Namespace is none
+		0x01, 0x00, 0x00, 0x00, // Name = 1
+	}
+	buf := bytes.NewReader(input)
+	sr := io.NewSectionReader(buf, 0, int64(len(input)))
+
+	f := new(XMLFile)
+	f.stringPool = new(ResStringPool)
+	f.stringPool.Strings = []string{"", "name"}
+	err := f.readEndElement(sr)
+
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+
+	if actual := f.xmlBuffer.String(); actual != "</name>" {
+		t.Errorf("got %v want </name>", actual)
 	}
 }
