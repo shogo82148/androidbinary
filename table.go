92b55dc13e9f7cc6ec889d1471f969e54c598052
Ichinose Shogo <shogo82148@gmail.com>
2014-05-06 17:11:47 +0900

    リソース読み込みのコードを分離

---
+++ table.go
@@ -0,0 +1,223 @@
+package androidbinary
+
+import (
+	"encoding/binary"
+	"io"
+	"os"
+)
+
+type TableFile struct {
+	stringPool    *ResStringPool
+	tablePackages []*TablePackage
+}
+
+type ResTableHeader struct {
+	Header       ResChunkHeader
+	PackageCount uint32
+}
+
+type ResTablePackage struct {
+	Header         ResChunkHeader
+	Id             uint32
+	Name           [128]uint16
+	TypeStrings    uint32
+	LastPublicType uint32
+	KeyStrings     uint32
+	LastPublicKey  uint32
+}
+
+type TablePackage struct {
+	Header      ResTablePackage
+	TypeStrings *ResStringPool
+	KeyStrings  *ResStringPool
+	TableTypes  []*TableType
+}
+
+type ResTableType struct {
+	Header       ResChunkHeader
+	Id           uint8
+	Res0         uint8
+	Res1         uint16
+	EntryCount   uint32
+	EntriesStart uint32
+	Config       ResTableConfig
+}
+
+type ResTableConfig struct {
+	Size   uint32
+	Imsi   uint32
+	Locale struct {
+		Language [2]uint8
+		Country  [2]uint8
+	}
+	ScreenType   uint32
+	Input        uint32
+	ScreenSize   uint32
+	Version      uint32
+	ScreenConfig uint32
+}
+
+type TableType struct {
+	Header  *ResTableType
+	Entries []TableEntry
+}
+
+type ResTableEntry struct {
+	Size  uint16
+	Flags uint16
+	Key   ResStringPoolRef
+}
+
+type TableEntry struct {
+	Key   *ResTableEntry
+	Value *ResValue
+	Flags uint32
+}
+
+type ResTableTypeSpec struct {
+	Header     ResChunkHeader
+	Id         uint8
+	Res0       uint8
+	Res1       uiont16
+	EntryCount uint32
+}
+
+func (f *TableFile) readTable(sr *io.SectionReader) error {
+	header := new(ResTableHeader)
+	binary.Read(sr, binary.LittleEndian, header)
+	f.tablePackages = make([]*TablePackage, header.PackageCount)
+
+	offset := int64(header.Header.HeaderSize)
+	for offset < int64(header.Header.Size) {
+		chunkHeader, err := f.readChunk(sr, offset)
+		if err != nil {
+			return err
+		}
+		offset += int64(chunkHeader.Size)
+	}
+	return nil
+}
+
+func (f *TableFile) readChunk(r io.ReaderAt, offset int64) (*ResChunkHeader, error) {
+	sr := io.NewSectionReader(r, offset, 1<<63-1-offset)
+	chunkHeader := &ResChunkHeader{}
+	sr.Seek(0, os.SEEK_SET)
+	if err := binary.Read(sr, binary.LittleEndian, chunkHeader); err != nil {
+		return nil, err
+	}
+
+	var err error
+	sr.Seek(0, os.SEEK_SET)
+	numTablePackages := 0
+	switch chunkHeader.Type {
+	case RES_TABLE_TYPE:
+		err = f.readTable(sr)
+	case RES_STRING_POOL_TYPE:
+		f.stringPool, err = ReadStringPool(sr)
+	case RES_TABLE_PACKAGE_TYPE:
+		var tablePackage *TablePackage
+		tablePackage, err = ReadTablePackage(sr)
+		f.tablePackages[numTablePackages] = tablePackage
+		numTablePackages++
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return chunkHeader, nil
+}
+
+func readTablePackage(sr *io.SectionReader) (*TablePackage, error) {
+	tablePackage := new(TablePackage)
+	header := new(ResTablePackage)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return nil, err
+	}
+
+	srTypes := io.NewSectionReader(sr, int64(header.TypeStrings), int64(header.Header.Size-header.TypeStrings))
+	if typeStrings, err := readStringPool(srTypes); err == nil {
+		tablePackage.TypeStrings = typeStrings
+	} else {
+		return nil, err
+	}
+
+	srKeys := io.NewSectionReader(sr, int64(header.KeyStrings), int64(header.Header.Size-header.KeyStrings))
+	if keyStrings, err := readStringPool(srKeys); err == nil {
+		tablePackage.KeyStrings = keyStrings
+	} else {
+		return nil, err
+	}
+
+	offset := int64(header.Header.HeaderSize)
+	for offset < int64(header.Header.Size) {
+		chunkHeader := &ResChunkHeader{}
+		sr.Seek(offset, os.SEEK_SET)
+		if err := binary.Read(sr, binary.LittleEndian, chunkHeader); err != nil {
+			return nil, err
+		}
+
+		var err error
+		chunkReader := io.NewSectionReader(sr, offset, int64(chunkHeader.Size))
+		sr.Seek(offset, os.SEEK_SET)
+		switch chunkHeader.Type {
+		case RES_TABLE_TYPE_TYPE:
+			var tableType *TableType
+			tableType, err = readTableType(chunkReader)
+			tablePackage.TableTypes = append(tablePackage.TableTypes, tableType)
+		case RES_TABLE_TYPE_SPEC_TYPE:
+			_, err = readTableTypeSpec(chunkReader)
+		}
+		if err != nil {
+			return nil, err
+		}
+		offset += int64(chunkHeader.Size)
+	}
+
+	return tablePackage, nil
+}
+
+func readTableType(sr *io.SectionReader) (*TableType, error) {
+	header := new(ResTableType)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return nil, err
+	}
+
+	entryIndexes := make([]uint32, header.EntryCount)
+	sr.Seek(int64(header.Header.HeaderSize), os.SEEK_SET)
+	if err := binary.Read(sr, binary.LittleEndian, entryIndexes); err != nil {
+		return nil, err
+	}
+
+	entries := make([]TableEntry, header.EntryCount)
+	for i, index := range entryIndexes {
+		if index == 0xFFFFFFFF {
+			continue
+		}
+		sr.Seek(int64(header.EntriesStart+index), os.SEEK_SET)
+		var key ResTableEntry
+		binary.Read(sr, binary.LittleEndian, &key)
+		entries[i].Key = &key
+
+		var val ResValue
+		binary.Read(sr, binary.LittleEndian, &val)
+		entries[i].Value = &val
+	}
+	return &TableType{
+		header,
+		entries,
+	}, nil
+}
+
+func readTableTypeSpec(sr *io.SectionReader) ([]uint32, error) {
+	header := new(ResTableTypeSpec)
+	if err := binary.Read(sr, binary.LittleEndian, header); err != nil {
+		return nil, err
+	}
+
+	flags := make([]uint32, header.EntryCount)
+	sr.Seek(int64(header.Header.HeaderSize), os.SEEK_SET)
+	if err := binary.Read(sr, binary.LittleEndian, flags); err != nil {
+		return nil, err
+	}
+	return flags, nil
+}
