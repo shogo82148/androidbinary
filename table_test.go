bf2cfabc1074eb36fa2895e50b48263045c36cce
Ichinose Shogo <shogo82148@gmail.com>
2014-05-11 17:26:37 +0900

    IsBetterThanのテスト追加

--- table_test.go
+++ table_test.go
@@ -219,3 +219,220 @@ func TestIsMoreSpecificThan(t *testing.T) {
 		}
 	}
 }
+
+func TestIsBetterThan_request_is_nil(t *testing.T) {
+	// a.IsBetterThan(b, nil) is same as a.IsMoreSpecificThan(b)
+	for _, tt := range isMoreSpecificThanTests {
+		actual := tt.me.IsBetterThan(tt.other, nil)
+		if actual != tt.expected {
+			if tt.expected {
+				t.Errorf("%v is better than %v, but get false", tt.me, tt.other)
+			} else {
+				t.Errorf("%v is better than %v, but get true", tt.me, tt.other)
+			}
+		}
+
+		if tt.expected {
+			// If 'me' is more specific than 'other', 'other' is not more specific than 'me'
+			if tt.other.IsBetterThan(tt.me, nil) {
+				t.Errorf("%v is better than %v, but get true", tt.other, tt.me)
+			}
+		}
+	}
+}
+
+var isBetterThanTests = []struct {
+	me       *ResTableConfig
+	other    *ResTableConfig
+	require  *ResTableConfig
+	expected bool
+}{
+	{
+		me:       &ResTableConfig{},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{Mcc: 1},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{Mcc: 1},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{Mcc: 1},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{Mcc: 1, Mnc: 1},
+		other:    &ResTableConfig{Mcc: 1},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{Language: [2]byte{'j', 'a'}},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{Language: [2]byte{'j', 'a'}},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{Language: [2]byte{'j', 'a'}},
+		expected: true,
+	},
+	{
+		me: &ResTableConfig{
+			Language: [2]uint8{'j', 'a'},
+			Country:  [2]uint8{'J', 'P'},
+		},
+		other: &ResTableConfig{
+			Language: [2]uint8{'j', 'a'},
+		},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me: &ResTableConfig{
+			Language: [2]uint8{'j', 'a'},
+			Country:  [2]uint8{'J', 'P'},
+		},
+		other: &ResTableConfig{
+			Language: [2]uint8{'j', 'a'},
+		},
+		require: &ResTableConfig{
+			Language: [2]uint8{'j', 'a'},
+			Country:  [2]uint8{'J', 'P'},
+		},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: SCREENSIZE_NORMAL},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: SCREENSIZE_NORMAL},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{ScreenLayout: SCREENSIZE_NORMAL},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{},
+		other:    &ResTableConfig{ScreenLayout: SCREENSIZE_SMALL},
+		require:  &ResTableConfig{ScreenLayout: SCREENSIZE_XLARGE},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: SCREENSIZE_SMALL},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{ScreenLayout: SCREENSIZE_SMALL},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: SCREENLONG_YES},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: SCREENLONG_YES},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{ScreenLayout: SCREENLONG_YES},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenLayout: LAYOUTDIR_LTR},
+		other:    &ResTableConfig{ScreenLayout: LAYOUTDIR_ANY},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{SmallestScreenWidthDp: 72},
+		other:    &ResTableConfig{SmallestScreenWidthDp: 71},
+		require:  &ResTableConfig{SmallestScreenWidthDp: 72},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenWidthDp: 100},
+		other:    &ResTableConfig{ScreenWidthDp: 99},
+		require:  &ResTableConfig{ScreenWidthDp: 100},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenHeightDp: 100},
+		other:    &ResTableConfig{ScreenHeightDp: 99},
+		require:  &ResTableConfig{ScreenHeightDp: 100},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{Orientation: 1},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{Orientation: 1},
+		other:    &ResTableConfig{},
+		require:  &ResTableConfig{Orientation: 1},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenWidth: 100},
+		other:    &ResTableConfig{ScreenWidth: 99},
+		require:  &ResTableConfig{ScreenWidth: 100},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{ScreenHeight: 100},
+		other:    &ResTableConfig{ScreenHeight: 99},
+		require:  &ResTableConfig{ScreenHeight: 100},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{SDKVersion: 2},
+		other:    &ResTableConfig{SDKVersion: 1},
+		require:  &ResTableConfig{},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{SDKVersion: 2},
+		other:    &ResTableConfig{SDKVersion: 1},
+		require:  &ResTableConfig{SDKVersion: 1},
+		expected: true,
+	},
+	{
+		me:       &ResTableConfig{SDKVersion: 1, MinorVersion: 1},
+		other:    &ResTableConfig{SDKVersion: 1},
+		require:  &ResTableConfig{SDKVersion: 1},
+		expected: false,
+	},
+	{
+		me:       &ResTableConfig{SDKVersion: 1, MinorVersion: 1},
+		other:    &ResTableConfig{SDKVersion: 1},
+		require:  &ResTableConfig{SDKVersion: 1, MinorVersion: 1},
+		expected: true,
+	},
+}
+
+func TestIsBetterThan(t *testing.T) {
+	for _, tt := range isBetterThanTests {
+		actual := tt.me.IsBetterThan(tt.other, tt.require)
+		if actual != tt.expected {
+			if tt.expected {
+				t.Errorf("%v is better than %v, but get false", tt.me, tt.other)
+			} else {
+				t.Errorf("%v is not better than %v, but get true", tt.me, tt.other)
+			}
+		}
+
+		if tt.expected {
+			// If 'me' is better than 'other', 'other' is not better than 'me'
+			if tt.other.IsBetterThan(tt.me, tt.require) {
+				t.Errorf("%v is not better than %v, but get true", tt.other, tt.me)
+			}
+		}
+	}
+}
