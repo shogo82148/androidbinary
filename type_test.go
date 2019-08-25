cd0019799f45ad77e84e47dbb816126d16a67faa
Ichinose Shogo <shogo82148@gmail.com>
2019-08-25 18:43:51 +0900

    add tests

---
+++ type_test.go
@@ -0,0 +1,83 @@
+package androidbinary
+
+import (
+	"encoding/xml"
+	"os"
+	"testing"
+)
+
+func TestBool(t *testing.T) {
+	type myMetaData struct {
+		Name  string `xml:"http://schemas.android.com/apk/res/android name,attr"`
+		Value Bool   `xml:"http://schemas.android.com/apk/res/android value,attr"`
+	}
+	type myXMLApplication struct {
+		XMLName  xml.Name     `xml:"application"`
+		MetaData []myMetaData `xml:"meta-data"`
+	}
+	type myXMLManifest struct {
+		XMLName     xml.Name         `xml:"manifest"`
+		Application myXMLApplication `xml:"application"`
+	}
+
+	f, err := os.Open("testdata/MyApplication/AndroidManifest.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	xmlFile, err := NewXMLFile(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	decoder := xml.NewDecoder(xmlFile.Reader())
+	xmlManifest := new(myXMLManifest)
+	err = decoder.Decode(xmlManifest)
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+
+	arscFile, err := os.Open("testdata/MyApplication/resources.arsc")
+	if err != nil {
+		t.Fatal(err)
+	}
+	arsc, err := NewTableFile(arscFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, data := range xmlManifest.Application.MetaData {
+		switch data.Name {
+		case "bool_test_true":
+			v, err := data.Value.WithTableFile(arsc).Bool()
+			if err != nil {
+				t.Error(err)
+			}
+			if !v {
+				t.Errorf("unexpected value: %v", v)
+			}
+		case "bool_test_false":
+			v, err := data.Value.WithTableFile(arsc).Bool()
+			if err != nil {
+				t.Error(err)
+			}
+			if v {
+				t.Errorf("unexpected value: %v", v)
+			}
+		case "bool_test_true_arsc":
+			v, err := data.Value.WithTableFile(arsc).Bool()
+			if err != nil {
+				t.Error(err)
+			}
+			if !v {
+				t.Errorf("unexpected value: %v", v)
+			}
+		case "bool_test_false_arsc":
+			v, err := data.Value.WithTableFile(arsc).Bool()
+			if err != nil {
+				t.Error(err)
+			}
+			if v {
+				t.Errorf("unexpected value: %v", v)
+			}
+		}
+	}
+}
