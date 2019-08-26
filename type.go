ed1f04e58185ce145dee960a3e22acae929f322e
Ichinose Shogo <shogo82148@gmail.com>
2019-08-27 07:10:00 +0900

    add Int32 type

--- type.go
+++ type.go
@@ -129,3 +129,84 @@ func (v Bool) MustBool() bool {
 	}
 	return ret
 }
+
+// Int32 is a boolean value in XML file.
+// It may be an immediate value or a reference.
+type Int32 struct {
+	value  string
+	table  *TableFile
+	config *ResTableConfig
+}
+
+// WithTableFile ties TableFile to the Bool.
+func (v Int32) WithTableFile(table *TableFile) Int32 {
+	return Int32{
+		value:  v.value,
+		table:  table,
+		config: v.config,
+	}
+}
+
+// WithResTableConfig ties ResTableConfig to the Bool.
+func (v Int32) WithResTableConfig(config *ResTableConfig) Bool {
+	return Bool{
+		value:  v.value,
+		table:  v.table,
+		config: config,
+	}
+}
+
+func (v *Int32) inject(table *TableFile, config *ResTableConfig) {
+	v.table = table
+	v.config = config
+}
+
+// SetInt32 sets an integer value.
+func (v *Int32) SetInt32(value int32) {
+	v.value = strconv.FormatInt(int64(value), 10)
+}
+
+// SetResID sets a boolean value with the resource id.
+func (v *Int32) SetResID(resID ResID) {
+	v.value = resID.String()
+}
+
+// UnmarshalXMLAttr implements xml.UnmarshalerAttr.
+func (v *Int32) UnmarshalXMLAttr(attr xml.Attr) error {
+	v.value = attr.Value
+	return nil
+}
+
+// Int32 returns the integer value.
+// It resolves the reference if needed.
+func (v Int32) Int32() (int32, error) {
+	if v.value == "" {
+		return 0, nil
+	}
+	if !IsResID(v.value) {
+		v, err := strconv.ParseInt(v.value, 10, 32)
+		return int32(v), err
+	}
+	id, err := ParseResID(v.value)
+	if err != nil {
+		return 0, err
+	}
+	value, err := v.table.GetResource(id, v.config)
+	if err != nil {
+		return 0, err
+	}
+	ret, ok := value.(uint32)
+	if !ok {
+		return 0, fmt.Errorf("invalid type: %T", value)
+	}
+	return int32(ret), nil
+}
+
+// MustInt32 is same as Int32, but it panics if it fails to parse the value.
+func (v Int32) MustInt32() int32 {
+	ret, err := v.Int32()
+	if err != nil {
+		panic(err)
+	}
+	return ret
+}
--- type_test.go
+++ type_test.go
@@ -81,3 +81,63 @@ func TestBool(t *testing.T) {
 		}
 	}
 }
+
+func TestInt32(t *testing.T) {
+	type myMetaData struct {
+		Name  string `xml:"http://schemas.android.com/apk/res/android name,attr"`
+		Value Int32  `xml:"http://schemas.android.com/apk/res/android value,attr"`
+	}
+	type myXMLApplication struct {
+		XMLName  xml.Name     `xml:"application"`
+		MetaData []myMetaData `xml:"meta-data"`
+	}
+	type myXMLManifest struct {
+		XMLName     xml.Name         `xml:"manifest"`
+		Application myXMLApplication `xml:"application"`
+	}
+
+	f, err := os.Open("testdata/MyApplication/AndroidManifest.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	xmlFile, err := NewXMLFile(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	arscFile, err := os.Open("testdata/MyApplication/resources.arsc")
+	if err != nil {
+		t.Fatal(err)
+	}
+	arsc, err := NewTableFile(arscFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	xmlManifest := new(myXMLManifest)
+	err = xmlFile.Decode(xmlManifest, arsc, nil)
+	if err != nil {
+		t.Errorf("got %v want no error", err)
+	}
+
+	for _, data := range xmlManifest.Application.MetaData {
+		switch data.Name {
+		case "int_test":
+			v, err := data.Value.Int32()
+			if err != nil {
+				t.Error(err)
+			}
+			if v != 42 {
+				t.Errorf("unexpected value: %v", v)
+			}
+		case "int_test_arsc":
+			v, err := data.Value.Int32()
+			if err != nil {
+				t.Error(err)
+			}
+			if v != -42 {
+				t.Errorf("unexpected value: %v", v)
+			}
+		}
+	}
+}
